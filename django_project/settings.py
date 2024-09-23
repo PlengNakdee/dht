ad8e2cc01aa6ffbedb7b891a98390cfe9739655f
PlengNakdee <plengwn@gmail.com>
2024-09-23 16:43:41 +0700

    modify setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,6 +131,9 @@ if not DEBUG:
     
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
---
+++ django_project/static/js/ds-card.js
@@ -0,0 +1,132 @@
+class DsCard extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.render();
+        this.updateSize();
+        this.updateState();
+        this.updateVariant();
+    }
+
+    render() {
+        const borderColor = this.getAttribute('border-color') || '#E0E1E5';
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host {
+                    display: block;
+                }
+                .card {
+                    border: 1px solid ${borderColor};
+                    border-radius: 8px;
+                    overflow: hidden;
+                }
+                .card.hover {
+                    border: none;
+                    box-shadow: 0px 9px 10px 0px rgba(57, 45, 123, 0.07);
+                }
+                .image-wrapper {
+                    text-align: center;
+                    margin: 16px;
+                }
+                .horizontal {
+                    display: flex;
+                    align-items: center;
+                }
+                .horizontal .image-wrapper {
+                    margin-right: 16px;
+                }
+                .card img {
+                    width: 100%;
+                    display: block;
+                }
+                .content {
+                    flex-grow: 1;
+                }
+                .vertical .title {
+                    padding-left: 16px;
+                }
+                .vertical .description {
+                    padding-left: 16px;
+                }
+                .vertical .subtitle-wrapper {
+                    padding-left: 16px;
+                }
+                .title-wrapper {
+                    display: flex;
+                }
+                .title {
+                    font-size: 16px;
+                    font-weight: 500;
+                    color: #392D7B;
+                    margin: 0;
+                }
+                .description {
+                    font-size: 14px;
+                    font-weight: 400;
+                    color: #9A99A7;
+                    margin: 0 0 12px 0;
+                }
+                .subtitle-wrapper {
+                    font-size: 12px;
+                    font-weight: 500;
+                    color: #5964AB;
+                    margin-bottom: 12px;
+                }
+                .icon-image-wrapper{
+                    padding-left: 6px;
+                }
+                .small, .md, .large, .md-deck {
+                    width: auto;
+                }
+                .sm { width: 280px; }
+                .md { width: 337px; }
+                .md-deck { width: 236px; }
+            </style>
+            <div class="card">
+                <div class="thumbnail-wrapper">
+                    <slot name="thumbnail"></slot>
+                </div>
+                <div class="content ${this.getAttribute('variant') === 'horizontal' ? 'horizontal' : 'vertical'}">
+                    <div class="image-wrapper">
+                        <slot name="image"></slot>
+                    </div>
+                    <div class="content-details">
+                    <div class="title-wrapper">
+                        <h2 class="title">${this.getAttribute('title') || 'Default Title'}</h2>
+                            <div class="icon-image-wrapper">
+                                <slot name="icon-image"></slot>
+                            </div>
+                    </div>
+                        <p class="description">${this.getAttribute('description') || 'No description provided.'}</p>
+                        <div class="subtitle-wrapper">
+                            <slot name="subtitle"></slot>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        `;
+    }
+
+    updateSize() {
+        const size = this.getAttribute('size') || 'md';
+        this.shadowRoot.querySelector('.card').classList.add(size);
+    }
+
+    updateState() {
+        const state = this.getAttribute('state');
+        if (state === 'hover') {
+            this.shadowRoot.querySelector('.card').classList.add('hover');
+        }
+    }
+
+    updateVariant() {
+        const variant = this.getAttribute('variant');
+        const content = this.shadowRoot.querySelector('.content');
+        content.classList.toggle('horizontal', variant === 'horizontal');
+        content.classList.toggle('vertical', variant !== 'horizontal');
+    }
+}
+
+customElements.define('ds-card', DsCard);
